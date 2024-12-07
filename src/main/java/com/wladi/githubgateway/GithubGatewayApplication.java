31a7c8ec6de0ed17da51e079d2b9b574b6180b74
Wladimir <wladibahia@gmail.com>
2024-12-07 13:16:45 -0300

    chore: configura e adiciona as dependencias necessarias para a aplicação.

--- src/main/java/com/wladi/githubgateway/GithubGatewayApplication.java
+++ src/main/java/com/wladi/githubgateway/GithubGatewayApplication.java
@@ -2,8 +2,10 @@ package com.wladi.githubgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GithubGatewayApplication {
 
 	public static void main(String[] args) {
