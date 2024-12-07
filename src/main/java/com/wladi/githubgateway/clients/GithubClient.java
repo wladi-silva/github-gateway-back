4cad4f81f4d0d6ef886e42d358470ce3a3abfa82
Wladimir <wladibahia@gmail.com>
2024-12-07 14:48:50 -0300

    feat: inicia os modelos de dados com alguns atributos, escolhido pojo para diminuir as dependencias ao framework e aumentar a flexibilidade.

--- src/main/java/com/wladi/githubgateway/clients/GithubClient.java
+++ src/main/java/com/wladi/githubgateway/clients/GithubClient.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.wladi.githubgateway.configurations.FeignConfiguration;
+import com.wladi.githubgateway.models.Repository;
+import com.wladi.githubgateway.models.User;
 
 @FeignClient(name = "githubClient", url = "https://api.github.com", configuration = FeignConfiguration.class)
 public interface GithubClient {
 
 
     @GetMapping("/users/{username}")
-    List<Object> getUser(@PathVariable String username);
+    User getUser(@PathVariable String username);
 
     @GetMapping("/users/{username}/repos")
-    List<Object> getUserRepositories(@PathVariable String username);
+    List<Repository> getUserRepositories(@PathVariable String username);
 
 }
---
+++ src/main/java/com/wladi/githubgateway/models/Repository.java
@@ -0,0 +1,39 @@
+package com.wladi.githubgateway.models;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Repository {
+
+    private String name;
+    private String url;
+    private String description;
+
+    public Repository() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
