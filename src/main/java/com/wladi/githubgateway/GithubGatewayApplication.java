bde4bb9091b63b86628acae4530a430f7b77d920
Wladimir <wladibahia@gmail.com>
2024-12-07 12:53:37 -0300

    feat: cria a estrutura do projeto.

---
+++ src/main/java/com/wladi/githubgateway/GithubGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wladi.githubgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wladi/githubgateway/GithubGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wladi.githubgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
